3500daadc7c6424260caf4241067772a588c40e9
GARIMA PANDEY <garimapandey@GARIMAs-MacBook-Air.local>
2023-01-08 03:34:03 -0500

    add initial project setup files to git

---
+++ resources/js/components/App.js
@@ -0,0 +1,17 @@
+import { applyStyles } from '@popperjs/core';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+               
+            </div>
+        </div>
+    );
+}
+
+
+export default App;
